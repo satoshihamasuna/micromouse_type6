a10d8b9710a987935f3340b444862585b47c95c1
sato1999shi <sato1999shi@gmail.com>
2023-06-23 14:22:52 +0900

    fix search

--- mouse_type6/Core/CPP/Inc/search_class.h
+++ mouse_type6/Core/CPP/Inc/search_class.h
@@ -21,7 +21,10 @@ class Search
 		make_map   *map_property;
 		motion_plan *motion_planning;
 	public:
-		t_position search_adachi_1(t_position start_pos,wall_class *_wall,make_map *_map,motion_plan *_motion);
+		t_position search_adachi_1(	t_position start_pos,	t_position goal_pos,	int goal_size,
+									wall_class *_wall,		make_map *_map,			motion_plan *_motion);
+		t_bool i_am_goal(int x,int y,int gx,int gy,int goal_size);
+		t_bool i_am_goal(t_position pos,t_position g_pos,int goal_size);
 
 };
 
--- mouse_type6/Core/CPP/Src/search.cpp
+++ mouse_type6/Core/CPP/Src/search.cpp
@@ -13,18 +13,36 @@
 #include "search_class.h"
 #include "adachi_class.h"
 //#include "glob_var_machine.h"
-t_bool i_am_goal(int x,int y,int *gx,int *gy,int goal_size){
+t_bool Search::i_am_goal(int x,int y,int gx,int gy,int goal_size){
 	t_bool flag = False;
-	for (int i = 0; i < goal_size;i++){
-		for(int j = 0; j < goal_size;j++){
-			if(x == gx[i] && y == gy[j]) flag = True;
+	if(gx <= x && x < (gx + goal_size))
+	{
+		if(gy <= y && y < (gy + goal_size))
+		{
+			flag = True;
 		}
 	}
 	return flag;
 }
 
+t_bool Search::i_am_goal(t_position pos,t_position g_pos,int goal_size)
+{
+	int x = pos.x,		y = pos.y;
+	int gx = g_pos.x,	gy = g_pos.y;
 
-t_position Search::search_adachi_1(t_position start_pos,wall_class *_wall,make_map *_map,motion_plan *_motion)
+	t_bool flag = False;
+	if(gx <= x && x < (gx + goal_size))
+	{
+		if(gy <= y && y < (gy + goal_size))
+		{
+			flag = True;
+		}
+	}
+	return flag;
+}
+
+t_position Search::search_adachi_1(	t_position start_pos,t_position goal_pos,int goal_size,
+									wall_class *_wall,make_map *_map,motion_plan *_motion)
 {
 	t_position tmp_my_pos = start_pos;
 	t_position my_position = tmp_my_pos;
@@ -32,6 +50,7 @@ t_position Search::search_adachi_1(t_position start_pos,wall_class *_wall,make_m
 	adachi search_algolithm(_wall, _map);
 
 	int direction = search_algolithm.get_next_dir(my_position, 0x01, &tmp_my_pos);
+	my_position = tmp_my_pos;
 	switch(direction)
 	{
 		case front:
@@ -45,4 +64,25 @@ t_position Search::search_adachi_1(t_position start_pos,wall_class *_wall,make_m
 
 	}
 
+	while(i_am_goal(my_position, goal_pos, goal_size) != True)
+	{
+		direction = search_algolithm.get_next_dir(my_position, 0x01, &tmp_my_pos);
+		if(_wall->is_unknown(my_position.x, my_position.y))
+		{
+				_wall->set_wall(my_position);
+		}
+		my_position = tmp_my_pos;
+		switch(direction)
+		{
+			case front:
+				break;
+			case right:
+				break;
+			case left:
+				break;
+			case rear:
+				break;
+		}
+	}
+	return my_position;
 }
